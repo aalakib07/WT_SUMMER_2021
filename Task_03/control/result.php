6c6bc02f755aa6cc815960d3434870401218082f
Abdullah Al Akib <aalakib07@gmail.com>
2021-07-02 21:10:40 +0600

    Add files via upload

--- Task_03/control/result.php
+++ Task_03/control/result.php
@@ -1,91 +1,119 @@
 <?php
-
-$validatename="";
-$validateemail="";
+$Name="";
+$email="";
+$Comment="";
 $validatepassword="";
 $validatecomment="";
-$validategender="";
-$validatehobby="";
-$validatefile="";
-$name=$email=$gender=$password=$comment="";
+$validateradio="";
+$validatecheckbox="";
 $v1=$v2=$v3="";
+$validateemail="";
+$Password="";
+$validFile="";
 
-
-if(isset($_POST['button']))
+if($_SERVER["REQUEST_METHOD"]=="POST")
 {
-    $name=$_REQUEST["fname"];
-    $email=$_REQUEST["email"];
-    $password=$_REQUEST["password"];
-    $comment=$_REQUEST["comment"];
-    $gender=$_REQUEST["gender"];
+$Name=$_REQUEST["name"]; 
+$email=$_REQUEST["email"]; 
+$Comment=$_REQUEST["comment"];
+$Password=$_REQUEST["password"];
 
-    if(empty($name))
-    {
-        $validatename= "Please Enter Your Name";
-    }
-    else if ((strlen($name)<3))
-    {
-        $validatename= "Please Enter at least 3 characters";
-    }
+if(!empty($Name) && strlen($Name)>=5) 
+{
+    $Name="You are ".$Name;
+}
 
-    if(empty($email))
-    {
-        $validateemail="Please Enter Your Email";
-    }
-    else if (!preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix",$email))
-    {
-        $validateemail= "Invalid Email";
-    }
-
-    if(empty($password))
-    {
-        $validatepassword= "Please Enter Your Password";
-    }
-    else if ((strlen($password)<8))
-    {
-        $validatepassword= "Please Enter Atleast 8 Characters";
-    }
-
-    if(empty($comment) || (strlen($comment)<20))
-    {
-        $validatecomment= "Please Enter Atleast 20 Characters";
-
-    }
-    else
-    {
-        $comment=$_REQUEST["comment"];
-    }
-
-    if(isset($_REQUEST["gender"]))
-    {
-        $validategender= $_REQUEST["gender"];
-    }
-    else
-    {
-        $validategender= "Please Select Your Gender";
-    }
-
-    if(!isset($_REQUEST["singing"]) && !isset($_REQUEST["dancing"]) && !isset($_REQUEST["reading"]))
-    {
-        $validatehobby = "Please Select at least one hobby";
-    }
-    else
-    {
-        if(isset($_REQUEST["singing"]))
-        {
-            $v1= $_REQUEST["singing"];
-        }
-        if(isset($_REQUEST["dancing"]))
-        { 
-            $v2= $_REQUEST["dancing"];
-        }
-        if(isset($_REQUEST["reading"]))
-        {
-            $v3= $_REQUEST["reading"];
-        }
-    }
-
+else if(empty($Name))
+{
+    $Name="Name Cant be empty !!";
+}
+else if( strlen($Name)<5)
+{
+    $Name="Your Name Must contain at least 4 character !!";
+}
+else if(empty($Name) &&  strlen($Name)<5)
+{
+    $Name="Invalid Name formate !!";
 }
 
 
-?>
+
+if(empty($email) || !preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix",$email))
+{
+    $validateemail="You Must Enter Valid Email";
+}
+else{
+    $validateemail= "Your Email is ".$email;
+}
+
+
+if(strlen($Password)<8)
+{
+    $validatepassword=" Password Must Contain 8 character!!";
+}
+else{
+    $validatepassword=$Password;
+}
+
+
+
+if(strlen($Comment)<20)
+{
+    $validatecomment=" Comment Must Contain 20 character!!";
+}
+else{
+    $validatecomment=$Comment;
+}
+
+if(isset($_REQUEST["gender"]))
+{
+    $validateradio= "Gender - ".$_REQUEST["gender"];
+}
+else{
+    $validateradio= "       Please Select Your Gender";
+}
+
+
+
+if(!isset($_REQUEST["Dancing"])&&!isset($_REQUEST["Singing"])&&!isset($_REQUEST["Reading"]))
+{
+    $validatecheckbox = "Please Select at Least One Checkbox";
+    
+}
+else{
+
+   $validatecheckbox="Your Selected Hobby : ";
+   if(isset($_REQUEST["Dancing"]))
+   {
+       $v1= $_REQUEST["Dancing"];
+       $validatecheckbox=$validatecheckbox.$v1;
+   }
+   if(isset($_REQUEST["Singing"]))
+   { 
+       $v2= $_REQUEST["Singing"];
+       $validatecheckbox=$validatecheckbox.",".$v2;
+   }
+   if(isset($_REQUEST["Reading"]))
+   {
+    $v3= $_REQUEST["Reading"];
+    $validatecheckbox=$validatecheckbox.",".$v3;
+   }
+   
+}
+
+$target_File="File/".$_FILES["fileupload"]["name"];
+
+if(move_uploaded_file($_FILES["fileupload"]["tmp_name"],$target_File))
+{
+    echo "You have uploadede a new file";
+   #echo "br".$_FILES(["fileupload"]["type"]);
+    #echo "<img src='".$target_File."'>";
+}
+else 
+    $validFile= "Sorry ,You must upload a file to continue";
+
+
+
+
+} 
+ ?>
